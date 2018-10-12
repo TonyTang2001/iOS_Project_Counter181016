4eb6af49d2ad82a1db86b0e599cea60df9fceb88
TonyTang2001 <tonytangdev@outlook.com>
2018-10-12 13:45:53 +0800

    Initial Commit

---
+++ TapCount/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TapCount
+//
+//  Created by 唐子轩 on 2018/10/12.
+//  Copyright © 2018 TonyTang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
